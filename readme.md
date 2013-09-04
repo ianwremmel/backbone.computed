500c9a640eb6b90271261620c42ac255e4249d15
Ian Remmel <design@ianwremmel.com>
2013-09-03 16:14:47 -1000

    initial commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Motivation
+
+Right now, things like fullname, roomEmailAddress, bookmarklet, etc, require special methods (and the knowledge of the existence of those methods). There are several existing plugins (or at least code samples) for doing this on the Internet, but the feeback from Derick Bailey and/or Jeremy Ashkenas was positive but hesitant.
+
+# Requirements:
+1. Trigger change event when dependent fields change
+2. Support assignment via ModelBinder.
+3. Trigger updates via ModelBinding.
+4. Use same syntax as regular attributes (e.g. model.get('computedAttr')).
+
+- Rather than building out a huge api for emitting events, add a `computedFields` hash to each model that contains a set of dependent fields and a compute function. Store the computed results via `set()`/`get() to trigger change events (this satisfies requirement 1, 3, and 4).
+- Point 2 is impossible in the general case and extremely complex in many of the specific cases. Until we have a use case for it, it's not worth doing.
+- The `computedFields` hash should support `includeInJSON` (`false` by default).
+- `set()` should not allow altering a computed attribute without also passing an `isComputed:true` option (this way, computed field will be kept as read only unless they've been recomputed.
+
+# TODO
+- Add a grunt UMD builder
+- Convert test suite to an actual test suite
+- Performance Testing
+- Add tests for ModelBinder
+- Add tests Backbone.Relational
+
+# Future
+- Investigate bi-directional computation (e.g. split a fullName into a firstName and a lastName)
---
+++ test/test.js
@@ -0,0 +1,134 @@
+// This test suite was written on an airplane (WOO!! HAWAII!!) without access to
+// NPM, so there was no way to get supporting files for an actual test library
+// (or for instructions for using same).
+
+'use strict';
+
+var Backbone = require('../bower_components/backbone/backbone.js');
+
+var attributes = {
+	firstName: 'first',
+	lastName: 'last',
+};
+
+var options = {
+	computedFields: {
+		fullName: {
+			fields: [
+				'firstName',
+				'lastName'
+			],
+			includeInJSON: false,
+			converter: function() {
+				return this.firstName + ' ' + this.lastName;
+			}
+		}
+	}
+};
+
+var model = new Backbone.Model(attributes, options);
+var pass = false;
+try {
+	model.set({fullName: 'FULLNAME'});
+} catch (e) {
+	pass = true;
+}
+if (!pass) {
+	throw new Error('attempting to set computedField without isComputed:true attribute should have thrown an Error');
+}
+
+
+model = new Backbone.Model(attributes, options);
+model.set({fullName: 'FULLNAME'}, {isComputed: true});
+if (model.get('fullName') !== 'FULLNAME') {
+	throw new Error('fullName should have been updated to FULLNAME but is instead "' + model.get('fullName') + '"');
+}
+
+
+model = new Backbone.Model(attributes, options);
+if (!model.has('fullName')) {
+	throw new Error('fullName should exist');
+}
+
+if (model.get('fullName') !== 'first last') {
+	throw new Error('fullName should equal "first last" but instead is "' + model.get('fullName') + '"');
+}
+
+pass = false;
+
+model.on('change', function(model, options) {
+	if (model.get('lastName') !== 'LAST') {
+		throw new Error('lastName should be "LAST" but was "' + model.get('lastName') + '"');
+	}
+
+	if (model.get('fullName') !== 'first LAST') {
+		throw new Error('fullName should be "first LAST" but was "' + model.get('fullName') + '"');
+	}
+
+	pass = true;
+});
+
+model.set({lastName: 'LAST'});
+
+if (!pass) {
+	throw new Error('change callback did not execute');
+}
+
+pass = false;
+model.on('change:fullName', function(model, attribute, options) {
+	if (attribute !== 'FIRST') {
+		throw new Error ('attribute should be "FIRST" but was "' + attribute + '"');
+	}
+});
+
+model.set({firstName: 'FIRST'});
+
+if (!pass) {
+	throw new Error('change:fullName callback did not execute');
+}
+
+// jshint eqeqeq:false
+var json = model.toJSON();
+if (json.firstName == null) {
+	throw new Error('firstName should be part of JSON');
+}
+
+if (json.lastName == null) {
+	throw new Error('lastName should be part of JSON');
+}
+
+if (json.fullName != null) {
+	throw new Error('fullName should not be part of JSON');
+}
+
+
+var options = {
+	computedFields: {
+		initials: {
+			fields: [
+				'firstName',
+				'lastName'
+			]
+		},
+		includeInJSON: true,
+		converter: function() {
+			return this.get('firstName').substr(0, 1) + this.get('lastName').substr(0,1);
+		}
+	},
+};
+
+var model = new Backbone.Model(attributes, options);
+var json = model.toJSON();
+if (json.firstName == null) {
+	throw new Error('firstName should be part of JSON');
+}
+
+if (json.lastName == null) {
+	throw new Error('lastName should be part of JSON');
+}
+
+if (json.fullName == null) {
+	throw new Error('fullName should be part of JSON');
+}
+// jshint eqeqeq:true
+
